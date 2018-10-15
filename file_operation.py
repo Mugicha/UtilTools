b026949d89c99631a7c05a0b7e48f9a1935f832a
Mugicha <Mugicha>
2018-10-15 16:11:58 +0900

    add txt_to_ary in file_operation.py

--- file_operation.py
+++ file_operation.py
@@ -10,3 +10,16 @@ class FileOperation():
         :return: DataFrame
         """
         return pd.read_csv(_path)
+
+    def txt_to_ary(self, _path: str):
+        """
+        対象のファイルを1行ごとに配列に格納してreturnする。
+        改行は配列に入れる際に削除される。
+        :param _path: 配列に格納したファイルのパス
+        :return: 1行1要素として格納された配列
+        """
+        _list = []
+        with open(_path) as f:
+            for line in f:
+                _list.append(line.replace('\n', ''))
+        return _list
