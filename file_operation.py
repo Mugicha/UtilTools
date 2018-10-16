2bc855a608c9ad94a054439a241c489170326b2c
Mugicha <dainiti.sukita@gmail.com>
2018-10-16 22:16:56 +0900

    modify csv_to_df.

--- file_operation.py
+++ file_operation.py
@@ -3,13 +3,27 @@ import pandas as pd
 
 class FileOperation():
 
-    def csv_to_df(self, _path: str):
+    def csv_to_df(self, _path: str, date_convert=False, date_format='YYYY-mm-dd', date_data_loc=0):
         """
         import csv and return the data as DataFrame.
         :param _path: csv path
+        :param date_convert: convert flag if the data contain date format data.
+        :param date_format: date data format e.g. YYYY-mm-dd
+        :param date_data_loc: date data column location in csv file.
         :return: DataFrame
         """
-        return pd.read_csv(_path)
+        if date_convert:
+            my_parser = lambda date: pd.datetime.strptime(date, date_format)
+            try:
+                return pd.read_csv(_path, parse_dates=[date_data_loc], date_parser=my_parser)
+            except:
+                print('Cannot import csv file or convert time format.')
+                exit(1)
+        try:
+            return pd.read_csv(_path)
+        except:
+            print('Cannot import csv file. [' + _path + ']')
+            exit(1)
 
     def txt_to_ary(self, _path: str):
         """
