aba32487668b0862a153ebabea29a95abe84a0bb
Mugicha <dainiti.sukita@gmail.com>
2018-10-16 20:12:59 +0900

    modify.

--- plot.py
+++ plot.py
@@ -1,20 +1,28 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import os
 
 
 class Plot():
 
-    def corr_map(self, _df: pd.DataFrame, path='./'):
+    def corr_map(self, _df: pd.DataFrame, _path='./'):
+        """
+        各変数の相関係数のヒートマップ画像を保存する機能。
+        :param _df: ヒートマップの基となるDataFrame.
+        :param _path: 保存するフォルダのパス
+        :return: None.
+        """
         plt.figure(figsize=(8, 6))  # heat map size
         sns.heatmap(_df.corr(), annot=True, cmap='plasma', linewidths=.5)
-        plt.savefig('corr_map.png')
+        plt.savefig(os.path.join(_path, 'corr_map.png'))
 
-    def scatter_with_histogram(self, _df: pd.DataFrame, dim_reduction=False):
+    def scatter_with_histogram(self, _df: pd.DataFrame, dim_reduction=False, _path='./'):
         """
         ヒストグラム付き散布図の画像を保存する機能。
         :param _df: 散布図の基となるDataFrame.
         :param dim_reduction: 次元削減するかどうか(default=False)
+        :param _path: 保存するフォルダのパス
         :return: None.
         """
         plt.figure(figsize=(8, 6))
@@ -26,14 +34,15 @@ class Plot():
             plt.savefig('scatter_with_histogram_pca.png')
             return None
         sns.jointplot(0, 1, _df, kind='scatter')
-        plt.savefig('scatter_with_histogram.png')
+        plt.savefig(os.path.join(_path, 'scatter_with_histogram.png'))
 
-    def pair_plot(self, _df: pd.DataFrame):
+    def pair_plot(self, _df: pd.DataFrame, _path='./'):
         """
         seabornのPariplot画像を保存する機能。
         :param _df: pairplotしたいDataFrame
+        :param _path: 保存するフォルダのパス
         :return: None.
         """
         plt.figure(figsize=(8, 6))
         sns.pairplot(_df)
-        plt.savefig('pair_plot.png')
+        plt.savefig(os.path.join(_path, 'pair_plot.png'))
