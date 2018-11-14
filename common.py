b5ac76f9d0e5a5d2ec589ab0378d9cf775bfdcce
Mugicha <dainiti.sukita@gmail.com>
2018-11-14 23:12:00 +0900

    mod

---
+++ common.py
@@ -0,0 +1,23 @@
+import os
+
+class Common:
+
+    def range_check(self, _param, _min, _max):
+        if _min <= _param <= _max:
+            return True
+        else:
+            return False
+
+    def folder_check(self, _path):
+        """
+        フォルダの存在確認をしてくれる機能
+        :param _path: 確認したいフォルダのパス
+        :return:
+        """
+        if not os.path.isdir(_path):
+            try:
+                os.mkdir(_path)
+                return True
+            except:
+                print('[common.py][Warn] Cannot create folder. ' + _path)
+                return False
--- image_processing.py
+++ image_processing.py
@@ -2,9 +2,12 @@ import cv2
 import os
 import numpy as np
 from tqdm import tqdm
+import UtilTools.common
 
 
 class Image_processing():
+    def __init__(self):
+        self.c = UtilTools.common.Common()
 
     def smooth_histogram(self, _path: str):
         """
@@ -22,18 +25,16 @@ class Image_processing():
 
         img2 = cdf[img]
         output_fol = '/'.join(_path.split('/')[0:-1]) + '/smoothed'
-        print(output_fol)
-        if not os.path.isdir(output_fol):
-            os.mkdir(output_fol)
-        output_img = ''.join(_path.split('/')[-1].split('.')[0:-1]) + '_smoothed.png'
-        print(output_fol + '/' + output_img)
-        cv2.imwrite(output_fol + '/' + output_img, img2)
+        if self.c.folder_check(output_fol):
+            output_img = ''.join(_path.split('/')[-1].split('.')[0:-1]) + '_smoothed.png'
+            cv2.imwrite(os.path.join(output_fol, '/' + output_img), img2)
 
-    def gamma_correction(self, _path: str, _gamma: float):
+    def gamma_correction(self, _path: str, _gamma: float, _outpath='./'):
         """
         ガンマ補正を行う機能。
         :param _path: 補正する画像のパス。
         :param _gamma: ガンマ値。
+        :param _outpath: 出力先のフォルダ
         :return:
         """
         img = cv2.imread(_path, 0)
@@ -46,34 +47,37 @@ class Image_processing():
         # cv2.imshow('corrected.', res)
         # cv2.waitKey(0)
         output_img = ''.join(_path.split('/')[-1].split('.')[0:-1]) + '_' + str(_gamma) + '.png'
-        cv2.imwrite(output_img, img_gamma)
+        if self.c.folder_check(_outpath):
+            cv2.imwrite(os.path.join(_outpath, output_img), img_gamma)
 
-    def extract_imgs(self, _h: int, _w: int, _path: str):
+    def extract_imgs(self, _h: int, _w: int, _path: str, _outpath='./'):
         """
         画像を定形に細切れにする機能。
         :param _h: 細切れにする高さ。
         :param _w: 細切れにする幅。
         :param _path: 細切れにする画像のパス。
+        :param _outpath: 細切れにした画像を保存するフォルダのパス。
         :return:
         """
         img = cv2.imread(_path, 0)
         if img.shape[0] % _h != 0 or img.shape[1] % _w != 0:
             print('[Warn] Image cannot be divided by _w, or _h.')
             exit(1)
+        if not self.c.folder_check(_outpath):
+            exit(1)
         for ew in range(0, len(img[1]), _w):
             for eh in range(0, len(img[0]), _h):
                 extracted_patch = img[eh:eh + _h - 1, ew:ew + _w - 1]
-                cv2.imwrite(
-                    './' + _path.split('/')[-1].split('.')[0] + '_extpatch_h' + str(eh) + '_w' + str(ew) + '.png',
-                    extracted_patch)
+                cv2.imwrite(os.path.join(_outpath, _path.split('/')[-1].split('.')[0] + '_extpatch_h' + str(eh) + '_w' + str(ew) + '.png'), extracted_patch)
 
-    def extract_img(self, _s: list, _h: int, _w: int, _path: str):
+    def extract_img(self, _s: list, _h: int, _w: int, _path: str, _outpath='./'):
         """
         画像から単一の画像を切り取る処理
         :param _s: 切り取る開始位置（リスト型変数で、[x,y]）
         :param _h: 切り取る画像の高さ
         :param _w: 切り取る画像の幅
         :param _path: 切り取る基の画像のパス
+        :param _outpath: 出力先のフォルダのパス
         :return:
         """
         if len(_s) != 2:
@@ -81,19 +85,17 @@ class Image_processing():
             exit(1)
         img = cv2.imread(_path, 0)
         ext_img = img[_s[0]:_s[0] + _h, _s[1]:_s[1] + _w]
-        output_fol = './test'
-        if not os.path.isdir(output_fol):
-            os.mkdir(output_fol)
-        cv2.imwrite(output_fol + _path.split('/')[-1].split('.')[0] + '_ext_h' + str(_h) + '_w' + str(_w) + '.png',
-                    ext_img)
+        if self.c.folder_check(_outpath):
+            cv2.imwrite(os.path.join(_outpath, _path.split('/')[-1].split('.')[0] + '_ext_h' + str(_h) + '_w' + str(_w) + '.png'), ext_img)
 
-    def extract_random_img(self, _num: int, _h: int, _w: int, _path: str):
+    def extract_random_img(self, _num: int, _h: int, _w: int, _path: str, _outpath='./'):
         """
         特定の画像から、決まったサイズの画像をランダムに切り取る処理
         :param _num: 切り取る画像の数
         :param _h: 切り取る画像の高さ
         :param _w: 切り取る画像の幅
-        :param _path: 切り取る画像のパス
+        :param _path: 切り取る画像ファイルのパス
+        :param _outpath: 出力先のフォルダのパス
         :return:
         """
         import random
@@ -102,4 +104,5 @@ class Image_processing():
             width = random.randint(0, len(img[0, :]) - _w)
             height = random.randint(0, len(img[:, 0]) - _h)
             ext_img = img[height:height + _h, width:width + _w]
-            cv2.imwrite('./preprocessing/ext_random/' + str(count) + '.png', ext_img)
+            if self.c.folder_check(_outpath):
+                cv2.imwrite(os.path.join(_outpath, str(count) + '.png'), ext_img)
